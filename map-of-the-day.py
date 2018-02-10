3154ce91f4f2b9000242f6d27885369af2adf2cb
Michael <mitch.heyman@gmail.com>
2018-02-10 15:17:25 -0800

    Add lat/long axes

--- map-of-the-day.py
+++ map-of-the-day.py
@@ -1,7 +1,22 @@
+
+# coding: utf-8
+
+# # Visualizing Map Data with Geopandas
+# 
+# There are several python packages that plot geographical data. In this example, we will be using [geopandas](http://geopandas.org/), because of its simplicity and its relation to `pandas`.
+# 
+# The data used in this example is provided by the US 2010 Census, and is hosted on the [US Census Bureau](https://www2.census.gov/) website.
+
+# ### Importing Python Packages
+# 
+# These packages are required for the following program to run. If unavailable, you can install them via `pip install <package-name>`.
+
+# In[1]:
+
+
 import requests
 import matplotlib.pyplot as plt
 import geopandas as gpd
-from mpl_toolkits.axes_grid1 import make_axes_locatable
 
 
 # ### Downloading Data
@@ -11,16 +26,16 @@ from mpl_toolkits.axes_grid1 import make_axes_locatable
 # In[2]:
 
 
-#url = 'http://www2.census.gov/geo/tiger/TIGER2010DP1/State_2010Census_DP1.zip'
+url = 'http://www2.census.gov/geo/tiger/TIGER2010DP1/State_2010Census_DP1.zip'
 file_name = 'State_2010Census_DP1.zip'
 
 
-## In[3]:
+# In[3]:
 
 
-#r = requests.get(url)
-#with open(f'{file_name}', 'wb') as f:
-#    f.write(r.content)
+r = requests.get(url)
+with open(f'{file_name}', 'wb') as f:
+    f.write(r.content)
 
 data = gpd.read_file(f"zip://{file_name}")
 
@@ -41,27 +56,23 @@ data.head()
 
 # ### Plotting the Default Map
 # 
-# This plots the raw map without any changes.
+# First, lets clean up the data a bit to make the visualization cleaner.
 
+# In[5]:
 
-plot_size = (50,20)
 
 data = data.drop([17]) # delete alaska
 data = data.drop([34]) # delete hawaii
 data = data.drop([7])  # delete puerto rico
 
 
-# The `normalize` function normalizes a list of numbers to between 0 and 1.
+# This plots the raw map without the previous changes.
+
+# In[18]:
 
 
-def normalize(lst):
-    max_num = max(lst)
-    min_num = min(lst)
-
-    for idx, num in enumerate(lst):
-        lst[idx] = (num - min_num) / (max_num - min_num)
-
-    return lst
+plot_size = (25,10)
+data.plot(figsize=plot_size)
 
 
 # ### Manipulating Data
@@ -78,10 +89,9 @@ for idx, row in data.iterrows():
     pop_dens_list.append(people_per_square_mile)
 
 
-# In[11]:
+# In[8]:
 
 
-#pop_dens_list = normalize(pop_dens_list)
 data['POPSQKM'] = pop_dens_list
 
 
@@ -90,18 +100,25 @@ data['POPSQKM'] = pop_dens_list
 # The one below is an example of the possible colormaps that can be fed into `cmap`.
 # 
 # ![colormaps](https://matplotlib.org/mpl_examples/color/colormaps_reference_02.png)
+# 
+# And finally, we add a color bar to the map.
 
-#data.plot(column='POPSQKM', cmap='Wistia', scheme='quantiles', figsize=plot_size)
+# In[32]:
+
 
 vmin, vmax = round(min(pop_dens_list),2), round(max(pop_dens_list),2)
 
-ax = data.plot(column='POPSQKM', cmap='Wistia', scheme='quantiles', figsize=plot_size)
+# plot the graph
+ax = data.plot(column='POPSQKM', cmap='Wistia', scheme='quantiles', figsize=(plot_size))
 
 # add colorbar
 fig = ax.get_figure()
-cax = fig.add_axes([0.9, 0.272, 0.03, 0.446])
+cax = fig.add_axes([0.8, 0.15, 0.015, 0.3])
 sm = plt.cm.ScalarMappable(cmap='Wistia', norm=plt.Normalize(vmin=vmin, vmax=vmax))
 sm._A = []
 fig.colorbar(sm, cax=cax)
 
-plt.show()
+# add latitude and longitude lines
+ax.grid('on', which='major', axis='x', linestyle='dashed', alpha=0.3)
+ax.grid('on', which='major', axis='y', linestyle='dashed', alpha=0.3)
+
